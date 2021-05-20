3b49a3575171d791ad48caf0c22c6b8c43636f03
Abhishek Das <abhid14@outlook.com>
2021-05-21 02:17:14 +0530

    scratch

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// framework7
+var $$ = Dom7;
+app = new Framework7({
+  root: "#app",
+  theme: "md",
+  routes,
+});
---
+++ js/routes.js
@@ -0,0 +1,7 @@
+var routes = [
+  { path: "/", url: "./index.html", name: "home" },
+  { path: "/index.html", url: "./index.html" },
+  { path: "/settings/", url: "./pages/settings.f7.html", name: "settings" },
+  { path: "(.*)", url: "./pages/404.f7.html" },
+];
+
